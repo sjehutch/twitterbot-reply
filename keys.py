152dc37bfb556f4b0047f01ae847c77a2387307a
scott hutchinson <sjehutch@me.com>
2017-02-20 06:55:08 -0600

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@
 #keys.py
 #visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
-	consumer_key =          '0ivZfeyfpg1eQsDETtxe9nNHj',
-	consumer_secret =       'Bn1YYo131hX25svUA5qK6qdrLz50D5odBpHR94D0A1NrMXscCU',
-	access_token =          '21925211-y8n28P0fpBgWG8aWz0YsQ9mRjJflYYaiDs2VRnsrq',
-	access_token_secret =   'KUkcqA7QmFMToV0iWuisTj5XiRAP3AVqrGUNtRWlsCK3p',
+	consumer_key =          'blah',
+	consumer_secret =       'blah',
+	access_token =          'blah',
+	access_token_secret =   'blah',
 )
